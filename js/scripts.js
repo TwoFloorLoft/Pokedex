07b9e3eb361c1cede84c061dd1130f6143475b76
TwoFloorLoft <joaoluisespiguinha@gmail.com>
2021-08-18 19:24:24 +0200

    Update scripts.js
    Pokemon List

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
-alert('Hello world');
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+// List of Pokemons
+
+let pokemonList = [
+  {
+    name: "Vulpix",
+    height: "0.6",
+    type: "fire"
+  },
+
+  {
+    name: "Gastly",
+    height: "1.3",
+    type: ["ghost","poison"]
+  },
+
+  {
+    name: "Psyduck",
+    height: "0.8",
+    type: "water"
+  },
+
+  {
+    name: "Pikachu",
+    height: "0.4",
+    type: "Electric"
+  },
+
+  {
+    name: "Mewtwo",
+    height: "2",
+    type: "Psychic"
+  }
+]
