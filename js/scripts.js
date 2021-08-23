5e595d3a334d9ff6271195f205f747b64ac5b0ed
TwoFloorLoft <joaoluisespiguinha@gmail.com>
2021-08-23 23:24:16 +0200

    Update scripts.js
    Created a loop for each pokemon and their respective height, plus a label if a pokemon is bigger than 1.5.

--- js/scripts.js
+++ js/scripts.js
@@ -3,31 +3,42 @@
 let pokemonList = [
   {
     name: "Vulpix",
-    height: "0.6",
+    height: " 0.6",
     type: "fire"
   },
 
   {
     name: "Gastly",
-    height: "1.3",
+    height: " 1.3",
     type: ["ghost","poison"]
   },
 
   {
     name: "Psyduck",
-    height: "0.8",
+    height: " 0.8",
     type: "water"
   },
 
   {
     name: "Pikachu",
-    height: "0.4",
+    height: " 0.4",
     type: "Electric"
   },
 
   {
     name: "Mewtwo",
-    height: "2",
+    height: " 2",
     type: "Psychic"
   }
-]
+];
+
+//a loop with each pokemon name and their respective height
+//a label if a pokemon is bigger than 1.5
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1.5) {
+    document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + ' -Wow that is big! </p>');
+  } else {
+    document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + '</p>');
+  }
+}
